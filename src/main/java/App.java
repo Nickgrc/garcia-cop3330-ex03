f5c8b76339fee672ea760d786834ccbd6299e4de
Nickolas Garcia <nickgrc@knights.ucf.edu>
2021-06-04 00:06:37 -0400

    Exercise 3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 /*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Nickolas Garcia
+ */
+
+/*
 Quotation marks are often used to denote the start and end of a string. But sometimes we need to print out the
 quotation marks themselves by using escape characters.
 
@@ -27,7 +32,7 @@ public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //input
-        
+
         System.out.print("What is the quote? ");
         String quote = in.nextLine();
 
